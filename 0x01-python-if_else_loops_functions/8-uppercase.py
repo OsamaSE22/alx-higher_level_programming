34649cd9f413d3ed480477603f55b4cc04f62482
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-08-01 21:41:17 +0300

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def uppercase(s):
+    npr = ""
     for i in s:
         if ord(i) >= 97 and ord(i) <= 122:
             nchar = ord(i) - 32
-            print(chr(nchar), end='')
-        elif ord(i) >= 65 and ord(i) <= 90:
-            print(i, end='')
+            npr += chr(nchar)
         else:
-            print(i, end='')
-    print()
+            npr += i
+    print(npr)
