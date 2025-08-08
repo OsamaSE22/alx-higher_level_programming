3509a25f9565714a4160c17126f567cb7b5ba6ad
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-08-09 01:41:09 +0300

    delete at

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx > (len(my_list) - 1) or idx < 0:
+        return my_list
+    new_list = []
+    for i in range(len(my_list)):
+        if i == idx:
+            continue
+        else:
+            new_list.append(my_list[i])
+    my_list[:] = new_list
+    return my_list
