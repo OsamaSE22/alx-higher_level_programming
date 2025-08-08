cedc0c1221a7a7a4eac1ece36a1fd7635cabc9e1
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-08-09 01:12:44 +0300

    is divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = my_list[:]
-    for element in range(0, (len(new_list) - 1)):
-        if (new_list[element] % 2 == 0):
-            new_list[element] = True
+    for i in range(len(new_list)):
+        if new_list[i] % 2 == 0:
+            new_list[i] = True
         else:
-            new_list[element] = False
+            new_list[i] = False
     return new_list
