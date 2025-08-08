e7b61e94afcad21919731ad2370b70ded87e11c7
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-08-09 00:16:42 +0300

    max element

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -11,7 +11,7 @@ def max_integer(my_list=[]):
                 result = my_list[i]
                 break
         if my_list[i] > my_list[i + 1] and my_list[i] > result:
-            result = my_listt[i]
+            result = my_list[i]
         elif my_list[i + 1] > my_list[i] and my_list[i + 1] > result:
             result = my_list[i + 1]
         elif result > my_list[i + 1] and result > my_list[i]:
