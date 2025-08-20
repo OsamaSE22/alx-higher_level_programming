4012af99ceab8a6f0e36623334d5c240fe914449
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-08-20 13:25:51 +0300

    convert roman number to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,19 +3,37 @@ def roman_to_int(roman_string):
     count = 0
     if roman_string != str(roman_string) or roman_string is None:
         return 0
-    for i in roman_string:
-        if i == 'X':
-            count += 10
-        elif i == 'V':
-            count += 5
-        elif i == 'I':
+    for i in range(len(roman_string)):
+        if roman_string[i] == 'X':
+            if roman_string[i - 1] == 'I':
+                count += 10 - 2
+            else:
+                count += 10
+        elif roman_string[i] == 'V':
+            if i > 0 and roman_string[i - 1] == 'I':
+                count += 5 - 2
+            else:
+                count += 5
+        elif roman_string[i] == 'I':
             count += 1
-        elif i == 'L':
-            count += 50
-        elif i == 'C':
-            count += 100
-        elif i == 'D':
-            count += 500
-        elif i == 'M':
-            count += 1000
+        elif roman_string[i] == 'L':
+            if roman_string[i - 1] == 'X':
+                count += 50 - 20
+            else:
+                count += 50
+        elif roman_string[i] == 'C':
+            if roman_string[i - 1] == 'X':
+                count += 100 - 20
+            else:
+                count += 100
+        elif roman_string[i] == 'D':
+            if roman_string[i - 1] == 'C':
+                count += 500 - 200
+            else:
+                count += 500
+        elif roman_string[i] == 'M':
+            if roman_string[i - 1] == 'C':
+                count += 1000 - 200
+            else:
+                count += 1000
     return count
