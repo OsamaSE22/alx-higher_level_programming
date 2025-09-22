743278a42626c06aa801fcbcd829759e15c3f808
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-09-22 22:04:06 +0300

    class with private data attribute and set function

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""this class is named Square.
+It as a private attribute called size.
+"""
+
+
+class Square:
+    """this is almost an empty class.
+    It just has the private attribute size
+    it's a part of alx tasks
+    """
+    def __init__(self, size=0):
+        """this is the constructor of this class.
+        It's automatically called when an object is created.
+        """
+        self.set_size(size)
+
+    def set_size(self, size):
+        """this method is to check the input.
+        It puts limitations of what kind of input is.
+        """
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
