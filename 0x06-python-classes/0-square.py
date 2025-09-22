699fa6604fb2131d1560e8a7dff07daf0f04cd80
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-09-22 21:34:10 +0300

    an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" this is the first module i made as a task in the oop tasks according to alx
-it's am empty class with no attribute or methods
+"""this is the first module i made as a task in the oop tasks according to alx
+it'sam empty class with no attribute or methods
 """
+
+
 class Square:
+
     """ this is an empty class
-    the name of the class is Square and it doesn't have any methods or data attributes
+    the name of the class is Square.
+    it doesn't have any methods or data attributes
     """
     pass
