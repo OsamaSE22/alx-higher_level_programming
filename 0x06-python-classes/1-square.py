4058ff7bcefd5c008ca08649523b9c7db23a54e9
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-09-22 21:45:54 +0300

    class with private data attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""this class is named Square.
+It as a private attribute called size.
+"""
+class Square:
+    """this is almost an empty class.
+    It just has the private attribute size
+    it's a part of alx tasks
+    """
+    def __init__(self, size):
+        """this is the constructor of this class.
+        It's automatically called when an object is created.
+        """
+        self.__size = size
