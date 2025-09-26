e3fcd57498ee2c6ee617aaa6c6638ec2fdb41b6e
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-09-26 20:50:45 +0300

    this is rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,7 +3,6 @@
 this is a module contain
 the definition of rectangle class
 """
-from pkg_resources import resource_isdir
 
 
 class Rectangle:
@@ -17,13 +16,16 @@ class Rectangle:
     {'_Rectangle__width': 10, '_Rectangle__height': 3}
     """
     def __init__(self, width=0, height=0):
+        """this is a constructor to define Rectangle."""
         self.width = width
         self.height = height
     @property
     def width(self):
+        """this is a property to define width Rectangle."""
         return self.__width
     @width.setter
     def width(self, value):
+        """this is a setter to define width Rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -32,9 +34,11 @@ class Rectangle:
             self.__width = value
     @property
     def height(self):
+        """this is a property to define height Rectangle."""
         return self.__height
     @height.setter
     def height(self, value):
+        """this is a setter to define height Rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
