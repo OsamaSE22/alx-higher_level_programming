14f9fbf179ba17f8edf21f23bb21f77f442b6b3e
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-09-26 21:06:30 +0300

    this is rectangle class

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+this is a module contain
+the definition of rectangle class
+>>> r = Rectangle(2, 4)
+    >>> print(r.__dict__)
+    {'_Rectangle__width': 2, '_Rectangle__height': 4}
+    >>> r.width = 10
+    >>> r.height = 3
+    >>> print(r.__dict__)
+    {'_Rectangle__width': 10, '_Rectangle__height': 3}
+    >>> r.width = 10
+    >>> r.height = 3
+    >>> print("Area: {} - Perimeter: {}".format(r.area(), r.perimeter()))
+    Area: 30 - Perimeter: 26
+    >>> r = Rectangle(2, 4)
+    >>> print("Area: {} - Perimeter: {}".format(r.area(), r.perimeter()))
+    Area: 8 - Perimeter: 12
+"""
+
+
+class Rectangle:
+    """this is a class to define Rectangle.
+    """
+    def __init__(self, width=0, height=0):
+        """this is a constructor to define Rectangle."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """this is a property to define width Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """this is a setter to define width Rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """this is a property to define height Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """this is a setter to define height Rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """this is a property to define area Rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """this is a property to define perimeter Rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return self.__width * 2 + self.__height * 2
