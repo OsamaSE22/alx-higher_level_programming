9a1fcef44cb170422938fb8a3fd27b2c61bec997
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-09-26 20:45:46 +0300

    this is rectangle class

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    count = 0
-    if roman_string != str(roman_string) or roman_string is None:
-        return 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    res = 0
+
     for i in range(len(roman_string)):
-        if roman_string[i] == 'X':
-            if i > 0 and roman_string[i - 1] == 'I':
-                count += 10 - 2
-            else:
-                count += 10
-        elif roman_string[i] == 'V':
-            if i > 0 and roman_string[i - 1] == 'I':
-                count += 5 - 2
-            else:
-                count += 5
-        elif roman_string[i] == 'I':
-            count += 1
-        elif roman_string[i] == 'L':
-            if i > 0 and roman_string[i - 1] == 'X':
-                count += 50 - 20
-            else:
-                count += 50
-        elif roman_string[i] == 'C':
-            if i > 0 and roman_string[i - 1] == 'X':
-                count += 100 - 20
-            else:
-                count += 100
-        elif roman_string[i] == 'D':
-            if i > 0 and roman_string[i - 1] == 'C':
-                count += 500 - 200
-            else:
-                count += 500
-        elif roman_string[i] == 'M':
-            if i > 0 and roman_string[i - 1] == 'C':
-                count += 1000 - 200
-            else:
-                count += 1000
-    return count
+        if i + 1 < len(roman_string) and roman[roman_string[i]] < roman[roman_string[i + 1]]:
+            res -= roman[roman_string[i]]
+        else:
+            res += roman[roman_string[i]]
+    return res
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+this is a module contain
+the definition of rectangle class
+"""
+from pkg_resources import resource_isdir
+
+
+class Rectangle:
+    """this is a class to define Rectangle.
+    >>> r = Rectangle(2, 4)
+    >>> print(r.__dict__)
+    {'_Rectangle__width': 2, '_Rectangle__height': 4}
+    >>> r.width = 10
+    >>> r.height = 3
+    >>> print(r.__dict__)
+    {'_Rectangle__width': 10, '_Rectangle__height': 3}
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
