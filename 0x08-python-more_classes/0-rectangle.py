e8253064d061db77d2b106fb322a7cc35a8804ca
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-09-26 20:21:28 +0300

    this is empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+this is a module contain
+empty class
+"""
+
+
 class Rectangle:
     """this is an empty class
     >>> r = Rectangle()
     >>> type(r)
     <class '0-rectangle.Rectangle'>
     >>> r.__dict__
-    {}
-    """
+    {}"""
     pass
