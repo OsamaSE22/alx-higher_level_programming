ff442f69f486dd437885157f31f8669f602250fb
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-10-14 18:25:31 +0300

    dir

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""this module has the lookup funciton with returns all methods and attributes about
+the class object"""
 def lookup(obj):
+    """return dir method """
     return dir(obj)
