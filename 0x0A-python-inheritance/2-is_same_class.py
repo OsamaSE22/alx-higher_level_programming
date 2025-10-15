51f505df99eb7e8d7231361d4f820a8d496be9ba
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-10-15 03:20:25 +0300

    is_instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,10 @@ the specified class"""
 
 def is_same_class(obj, a_class):
     """the func with return the bool value """
-    if isinstance(obj, a_class):
+    if (
+            isinstance(obj, a_class)
+            and not issubclass(type(obj), a_class) or type(obj) is a_class
+    ):
         return True
     else:
         return False
