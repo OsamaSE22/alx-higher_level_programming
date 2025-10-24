41d8fc840508ee8983c5e8cd4b81657a19fb47d3
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-10-24 20:30:52 +0300

    file i/o

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 '''this module contains a function that
 return a python object to a json string'''
+
+
 import json
-
-
-
 def to_json_string(my_obj):
     '''return a json representation to the python object'''
     return json.dumps(my_obj)
