2c18c36029b7401c6e31883ffb39d53a7892590e
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-10-24 14:26:24 +0300

    file i/o

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''this module contains a funciton to read txt files'''
+
+
 def read_file(filename=""):
+    '''pass the filename with its path and it will be read
+    by this funciton and printed'''
     with open(filename, encoding='utf-8') as f:
         print(f.read())
